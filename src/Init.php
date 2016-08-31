ddec4211e48232ba142a2608b0bc752b0e9ba859
cbiedermann <c.biedermann@web4train.de>
2016-08-31 09:38:47 +0200

    composer installed full featured

---
+++ src/Init.php
@@ -0,0 +1,19 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: cbiedermann
+	 * Date: 31.08.2016
+	 * Time: 08:21
+	 */
+
+	namespace Class152\PizzaMamamia;
+
+
+	class Init
+	{
+
+		public function __construct()
+		{
+			echo "Hallo Welt";
+		}
+	}
