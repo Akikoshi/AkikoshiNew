c0d514fd0e868caede153f85d8ee03c3303e4ea7
cbiedermann <c.biedermann@web4train.de>
2016-09-05 14:21:11 +0200

    the base exceptions for the project

---
+++ src/Exception/PizzaProjectException.php
@@ -0,0 +1,15 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: cbiedermann
+	 * Date: 05.09.2016
+	 * Time: 14:19
+	 */
+
+	namespace Class152\PizzaMamamia\Exception;
+
+
+	class PizzaProjectException extends \Exception
+	{
+
+	}
