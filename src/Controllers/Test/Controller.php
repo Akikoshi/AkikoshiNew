175fb0e3bbdcfe4d08a1e2f8806b0b77da9109a2
cbiedermann <c.biedermann@web4train.de>
2016-09-07 09:24:10 +0200

    abstract controller

---
+++ src/Controllers/Test/Controller.php
@@ -0,0 +1,29 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: cbiedermann
+	 * Date: 07.09.2016
+	 * Time: 09:12
+	 */
+
+	namespace Class152\PizzaMamamia\Controllers\Test;
+
+
+	use Class152\PizzaMamamia\AbstractClasses\AbstractController;
+	use Class152\PizzaMamamia\Library\TwigRendering;
+
+	class Controller extends AbstractController
+	{
+		public function indexAction()
+		{
+
+			new TwigRendering(
+				'Home/index.twig',
+				[
+					'controllerName'=>'Home',
+					'actionName' => 'index',
+				]
+			);
+
+		}
+	}
