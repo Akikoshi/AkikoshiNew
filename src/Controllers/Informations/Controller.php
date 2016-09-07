0556a88c103ce28abdb88e9bb6d7a02c7f8050bb
RauchR <Robert.Rauch@web4train.de>
2016-09-07 13:13:29 +0200

    add Information Controller

---
+++ src/Controllers/Informations/Controller.php
@@ -0,0 +1,36 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: cbiedermann
+	 * Date: 05.09.2016
+	 * Time: 14:30
+	 */
+
+	namespace Class152\PizzaMamamia\Controllers\Informations;
+
+
+	use Class152\PizzaMamamia\AbstractClasses\AbstractController;
+	use Class152\PizzaMamamia\Library\TwigRendering;
+
+
+	class Controller extends AbstractController
+	{
+		/**
+		 *
+         */
+		public function indexAction()
+		{
+
+			new TwigRendering(
+				'Informations/index.twig',
+				[
+					'controllerName'=>'Informations',
+					'actionName' => 'index',
+				]
+			);
+
+		}
+
+
+		
+	}
