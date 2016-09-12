72bcf70953c732cd1d2afffa4f8b3a5b425306ba
cbiedermann <c.biedermann@web4train.de>
2016-09-12 14:19:57 +0200

    MenuService as a example implementation

---
+++ src/Controllers/Test/Controller.php
@@ -0,0 +1,36 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: cbiedermann
+	 * Date: 07.09.2016
+	 * Time: 14:19
+	 */
+
+	namespace Class152\PizzaMamamia\Controllers\Test;
+
+
+	use Class152\PizzaMamamia\AbstractClasses\AbstractController;
+	use Class152\PizzaMamamia\Library\ControllerMenu;
+	use Class152\PizzaMamamia\Library\TwigRendering;
+	use Class152\PizzaMamamia\Services\MenuService\MenuService;
+
+	class Controller extends AbstractController
+	{
+
+		public function indexAction()
+		{
+			$menuService = new MenuService();
+			$controllerMenu = $menuService->getControllerMenu();
+
+			new TwigRendering(
+				'Test/index.twig',
+				[
+					'controllerName'=>'Test',
+					'actionName' => 'index',
+				  'mainMenu' => $controllerMenu,
+				]
+			);
+
+		}
+
+	}
