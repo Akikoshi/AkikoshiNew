ab5ae56514dd8810c8d0f6889ad3bae3fc3e6065
vieteo <oliver.viete@web4train.de>
2016-09-13 10:42:25 +0200

    Main Menu added

---
+++ src/Services/MenuService/Library/DbConnectorFake.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vieteo
+ * Date: 13.09.2016
+ * Time: 09:43
+ */
+
+namespace Class152\PizzaMamamia\Services\MenuService\Library;
+
+
+class DbConnectorFake
+{
+    
+}
---
+++ src/Services/MenuService/Library/MainMenuFactory.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vieteo
+ * Date: 13.09.2016
+ * Time: 09:40
+ */
+
+namespace Class152\PizzaMamamia\Services\MenuService\Library;
+
+
+class MainMenuFactory
+{
+    private $mainMenu;
+    
+    public function __construct()
+    {
+        $this->mainMenu = new MenuItemList();
+        
+        $this->mainMenu->addItem(
+            new MenuItem('Aktionen', '/campaigns/index')
+        );
+
+        $subMenu = new MenuItemList();
+        $subMenu->addItem(
+            new MenuItem('Alle einsehnen','/home/index?typ=1')
+        );
+        
+        $subMenu->addItem(
+            new MenuItem('Vegetarische Pizzas', '/home/index?typ=1&option=veg')
+        );
+        
+        $subMenu->addItem(
+            new MenuItem('Klassiker','/home/index?typ=1&option=kla')
+        );
+
+        $this->mainMenu->addItem(
+            new MenuItem('Pizza','/home/index?typ=1',
+                $subMenu)
+        );
+//---------------------------------------------------------------------------------------------------------------------------
+        $subMenu = new MenuItemList();
+        $subMenu->addItem(
+            new MenuItem('Alle einsehnen','/home/index?typ=2')
+        );
+
+        $subMenu->addItem(
+            new MenuItem('Vegetarische Burger', '/home/index?typ=2&option=veg')
+        );
+
+        $subMenu->addItem(
+            new MenuItem('Klassiker','/home/index?typ=2&option=kla')
+        );
+
+        $this->mainMenu->addItem(
+            new MenuItem('Burger','/home/index?typ=1',
+                $subMenu)
+        );
+    }
+    
+    public function getMianMenu()
+    {
+        return $this->mainMenu;
+    }
+}
