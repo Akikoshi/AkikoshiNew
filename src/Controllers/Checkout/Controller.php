af5974264e41c0e1db31dc54eac32ad8b24131ba
vieteo <oliver.viete@web4train.de>
2016-09-13 11:49:38 +0200

    Controller Checkout and Configurator changed

--- src/Controllers/Checkout/Controller.php
+++ src/Controllers/Checkout/Controller.php
@@ -11,26 +11,45 @@ namespace Class152\PizzaMamamia\Controllers\Checkout;
 
 use Class152\PizzaMamamia\AbstractClasses\AbstractController;
 use Class152\PizzaMamamia\Library\TwigRendering;
+use Class152\PizzaMamamia\Services\MenuService\MenuService;
 
 class Controller extends AbstractController
 {
 
+
+
+
     public function indexAction(){
+        $menuService = new MenuService();
+        $mainMenu = $menuService->getMainMenu();
+        $accountMenu = $menuService->getAccountMenu();
+        $footerMenu = $menuService->getFooterMenu();
+        
         new TwigRendering(
             'Checkout/index.twig',
             [
                 'controllerName'=>'Checkout',
                 'actionName' => 'index',
+                'mainMenu' => $mainMenu,
+                'footerMenu' => $footerMenu,
+                'accountMenu' => $accountMenu,
             ]
         );
     }
     
     public function confirmorderAction(){
+        +$menuService = new MenuService();
+        $mainMenu = $menuService->getMainMenu();
+        $accountMenu = $menuService->getAccountMenu();
+        $footerMenu = $menuService->getFooterMenu();
         new TwigRendering(
             'Checkout/confirmOrder.twig',
             [
                 'controllerName'=>'Checkout',
                 'actionName' => 'confirmOrder',
+                'mainMenu' => $mainMenu,
+                'footerMenu' => $footerMenu,
+                'accountMenu' => $accountMenu,
             ]
         );
     }
