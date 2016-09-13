77182eeaf2734f95a6ae8367b1e6a2d04f19a5c8
RauchR <Robert.Rauch@web4train.de>
2016-09-13 14:40:56 +0200

    merged development branch

--- src/Controllers/Campaigns/Controller.php
+++ src/Controllers/Campaigns/Controller.php
@@ -12,27 +12,56 @@ namespace Class152\PizzaMamamia\Controllers\Campaigns;
 use Class152\PizzaMamamia\AbstractClasses\AbstractController;
 use Class152\PizzaMamamia\Library\TwigRendering;
 
+use Class152\PizzaMamamia\Services\MenuService\MenuService;
+
 class Controller extends AbstractController
 {
+    
+    
+    
     public function indexAction()
     {
-        new TwigRendering(
+
+        $menuService = new MenuService();
+        $accountMenu = $menuService->getAccountMenu();
+        $footerMenu= $menuService->getFooterMenu();
+        $mainMenu=   $menuService->getMainMenu();
+        $campaignsMenu=   $menuService->getCampaignsMenu();
+        
+        
+        new TwigRendering( 
             'Campaigns/index.twig',
             [
                 'controllerName'=>'Campaigns',
                 'actionName' => 'index',
+                'mainMenu' => $mainMenu,
+                'footerMenu' => $footerMenu,
+                'accountMenu' => $accountMenu,
+                'campaignsMenu' => $campaignsMenu,  
             ]
         );
+   
     }
 
     public function detailAction()
     {
-	    new TwigRendering(
+        $menuService = new MenuService();
+        $accountMenu = $menuService->getAccountMenu();
+        $footerMenu= $menuService->getFooterMenu();
+        $mainMenu=   $menuService->getMainMenu();
+        $campaignsMenu=   $menuService->getCampaignsMenu();
+
+
+        new TwigRendering(
 		    'Campaigns/detail.twig',
 		    [
 			    'controllerName'=>'Campaigns',
 			    'actionName' => 'detail',
-		    ]
+                'mainMenu' => $mainMenu,
+                'footerMenu' => $footerMenu,
+                'accountMenu' => $accountMenu,
+                'campaignsMenu' => $campaignsMenu,
+            ]
 	    );
 
     }
