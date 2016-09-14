2340b6d6ee411b85e1d3dd255e3052193118ee58
vieteo <oliver.viete@web4train.de>
2016-09-14 15:40:43 +0200

    FilterList, SortList, ProductList, Output, and Paginator added,

--- src/Services/ProductListService/Library/ProductItem.php
+++ src/Services/ProductListService/Library/ProductItem.php
@@ -23,17 +23,22 @@ class ProductItem
     /** @var  string */
     private $detailUrl;
 
+    /** @var  float */
+    private $price;
+
     /** @param  string $name
      *  @param  string $description
      *  @param  string $pictureUrl
      *  @param  string $detailUrl
+     *  @param  float  $price
      */
-    public function __construct(string $name, string $description, string $pictureUrl, string $detailUrl)
+    public function __construct(string $name, string $description, string $pictureUrl, string $detailUrl, float $price)
     {
         $this->name = $name;
         $this->description = $description;
         $this->pictureUrl =  $pictureUrl;
         $this->detailUrl = $detailUrl;
+        $this->price = $price;
     }
 
     /** @return string */
@@ -58,5 +63,11 @@ class ProductItem
     public function getDetailUrl() : string
     {
         return $this->detailUrl;
+    }
+
+    /** @return float */
+    public function getPrice()
+    {
+        return number_format($this->price, 2, ',', '.');
     }
 }
---
+++ src/Services/ProductListService/Library/ProductListPaginator.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vieteo
+ * Date: 14.09.2016
+ * Time: 14:10
+ */
+
+namespace Class152\PizzaMamamia\Services\ProductListService\Library;
+
+
+class ProductListPaginator
+{
+    private $productFactory;
+
+    private $limit;
+    
+    private $total;
+
+    private $output;
+
+    public function __construct(int $limit)
+    {
+        $this->limit = $limit;
+        $this->productFactory = new ProductListFactory();
+        $this->total = count($this->productFactory->getProductList());
+    }
+
+    private function limitItems()
+    {
+        $counter = 0;
+        for($i = $counter; $i < $this->total; $i++ )
+        {
+            for ($j = 0; $j < $this->limit; $j++)
+            {
+                $counter = $j;
+            }
+        }
+    }
+
+    public function getProductList()
+    {
+        return $this->output;
+    }
+
+    public function hasNext()
+    {
+        return true;
+    }
+
+    public function getNextUrl()
+    {
+        return '/productlist/index';
+    }
+
+    public function hasBefore()
+    {
+        return true;
+    }
+
+    public function getBeforeUrl()
+    {
+        return '/productlist/index';
+    }
+
+}
