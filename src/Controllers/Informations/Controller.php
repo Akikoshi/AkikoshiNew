4e7ab7625f05a7c842c11bb9a085e22368f3c2bc
johannesj <jens.johannes@web4train.de>
2016-09-14 15:12:54 +0200

    fix a variablename changing

--- src/Controllers/Informations/Controller.php
+++ src/Controllers/Informations/Controller.php
@@ -42,6 +42,95 @@
 
 		}
 
+		public function gbtAction()
+		{
+
+			$menuService = new MenuService($this->request);
+			$mainMenu = $menuService->getMainMenu();
+			$accountMenu = $menuService->getAccountMenu();
+			$footerMenu = $menuService->getFooterMenu();
+			$breadcrumbMenu = $menuService->getBreadcrumbMenu();
+
+			new TwigRendering(
+				'Informations/gbt.twig',
+				[
+					'controllerName'=>'Informations',
+					'actionName' => 'gbt',
+					'mainMenu' => $mainMenu,
+					'footerMenu' => $footerMenu,
+					'accountMenu' => $accountMenu,
+					'breadcrumbMenu' => $breadcrumbMenu,
+				]
+			);
+
+		}
+		public function imprintAction()
+		{
+
+			$menuService = new MenuService($this->request);
+			$mainMenu = $menuService->getMainMenu();
+			$accountMenu = $menuService->getAccountMenu();
+			$footerMenu = $menuService->getFooterMenu();
+			$breadcrumbMenu = $menuService->getBreadcrumbMenu();
+
+			new TwigRendering(
+				'Informations/imprint.twig',
+				[
+					'controllerName'=>'Informations',
+					'actionName' => 'imprint',
+					'mainMenu' => $mainMenu,
+					'footerMenu' => $footerMenu,
+					'accountMenu' => $accountMenu,
+					'breadcrumbMenu' => $breadcrumbMenu,
+				]
+			);
+
+		}
+
+		public function contactAction()
+		{
+
+			$menuService = new MenuService($this->request);
+			$mainMenu = $menuService->getMainMenu();
+			$accountMenu = $menuService->getAccountMenu();
+			$footerMenu = $menuService->getFooterMenu();
+			$breadcrumbMenu = $menuService->getBreadcrumbMenu();
+
+			new TwigRendering(
+				'Informations/contact.twig',
+				[
+					'controllerName'=>'Informations',
+					'actionName' => 'contact',
+					'mainMenu' => $mainMenu,
+					'footerMenu' => $footerMenu,
+					'accountMenu' => $accountMenu,
+					'breadcrumbMenu' => $breadcrumbMenu,
+				]
+			);
+
+		}
+		public function qualityAction()
+		{
+
+			$menuService = new MenuService($this->request);
+			$mainMenu = $menuService->getMainMenu();
+			$accountMenu = $menuService->getAccountMenu();
+			$footerMenu = $menuService->getFooterMenu();
+			$breadcrumbMenu = $menuService->getBreadcrumbMenu();
+
+			new TwigRendering(
+				'Informations/quality.twig',
+				[
+					'controllerName'=>'Informations',
+					'actionName' => 'quality',
+					'mainMenu' => $mainMenu,
+					'footerMenu' => $footerMenu,
+					'accountMenu' => $accountMenu,
+					'breadcrumbMenu' => $breadcrumbMenu,
+				]
+			);
+
+		}
 
 		
 	}
