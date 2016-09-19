8706f8fe9fd72bc969159d46ad62c39f8fdef6b9
frankenfeldtp <peter.frankenfeldt@web4train.de>
2016-09-19 14:59:36 +0200

    MainMenuFactory [RC] : Implement recursive function to load the MainMenu from database. MenuRepository : Deactivate debug var_dumb.  vagrant.sql : Add new MainMenu-data to database.

--- src/Services/MenuService/Library/MainMenuFactory.php
+++ src/Services/MenuService/Library/MainMenuFactory.php
@@ -14,186 +14,235 @@ use Class152\PizzaMamamia\Services\MenuService\Repositories\MenuRepository;
 
 class MainMenuFactory
 {
-    private $mainMenu;
-    
-    public function __construct()
-    {
+	/**
+	 * @var MenuItemList
+	 */
+	private $mainMenu;
 
-    	  $repository = new MenuRepository();
+	/**
+	 * @var MenuRepository
+	 */
+	private $repository;
 
-	      $data = $repository->getMenuByName('mainMenu');
-	      // var_dump($data);
+	/**
+	 * MainMenuFactory constructor.
+	 */
+	public function __construct()
+	{
+
+		$this->repository = new MenuRepository();
+
+		$this->mainMenu = new MenuItemList();
+
+		$sqlResult = $this->repository->getMenuByName( 'mainMenu' );
+//		var_dump( $sqlResult );
+
+		$this->mainMenu = $this->getSubMenuItems( $sqlResult->getId() );
+//		var_dump($this->mainMenu);
+
+		/** @var MenuEntity $elem */
+		foreach ( $sqlResult as $elem ) {
+			$this->mainMenu->addItem( new MenuItem( $elem->getName(), $elem->getUrl() ) );
+		}
+
+		return;
 
 
-	      $this->mainMenu = new MenuItemList();
-
-	      $data = $repository->getMenuesByParentIds([
-	      	$data->getId(),
-	      ]);
-
-	      /** @var MenuEntity $elem */
-	    foreach( $data as $elem )
-	      {
-	      	$this->mainMenu->addItem( new MenuItem( $elem->getName(),$elem->getUrl() ) );
-	      }
-
-	      return;
+//		$this->mainMenu = new MenuItemList();
 
 
-        $this->mainMenu = new MenuItemList();
-        
-        $this->mainMenu->addItem(
-            new MenuItem('Aktionen', '/campaigns/index')
-        );
+	}
 
-        $subMenu = new MenuItemList();
-        $subMenu->addItem(
-            new MenuItem('Alle einsehnen', '/productlist/index')
-        );
-        
-        $subMenu->addItem(
-            new MenuItem('Vegetarische Pizzas', '/productlist/index')
-        );
-        
-        $subMenu->addItem(
-            new MenuItem('Klassiker', '/productlist/index')
-        );
+	public function getMainMenu()
+	{
+		return $this->mainMenu;
+	}
 
-        $this->mainMenu->addItem(
-            new MenuItem('Pizza', '/productlist/index',
-                $subMenu)
-        );
-//---------------------------------------------------------------------------------------------------------------------------
-        $subMenu = new MenuItemList();
-        $subMenu->addItem(
-            new MenuItem('Alle einsehnen', '/productlist/index')
-        );
+	/**
+	 * @param int $mId
+	 * @return MenuItemList
+	 * Recursive function to get all menu- and sub-menu-items
+	 */
+	private function getSubMenuItems( int $mId ) : MenuItemList
+	{
+		$result = new MenuItemList();
 
-        $subMenu->addItem(
-            new MenuItem('Vegetarische', '/productlist/index')
-        );
+		/* got all sub-elements of this menu-item (id) */
+		$sqlResult = $this->repository->getMenuesByParentIds( [ $mId ] );
 
-        $subMenu->addItem(
-            new MenuItem('Klassiker', '/productlist/index')
-        );
+		/** @var MenuEntity $elem */
+		foreach ( $sqlResult as $elem ) {
+			$id = $elem->getId();
+			$subItems = $this->getSubMenuItems( $id );      // Check and generate the Sub-ItemList
 
-        $subMenu->addItem(
-            new MenuItem('Überbackenes', '/productlist/index')
-        );
-
-        $subMenu->addItem(
-            new MenuItem('Saisonal', '/productlist/index')
-        );
-
-        $this->mainMenu->addItem(
-            new MenuItem('Nudeln', '/productlist/index',
-                $subMenu)
-        );
-//---------------------------------------------------------------------------------------------------------------------------
-        $subMenu = new MenuItemList();
-        $subMenu->addItem(
-            new MenuItem('Alle einsehnen', '/productlist/index')
-        );
-
-        $subMenu->addItem(
-            new MenuItem('Anti Pasti', '/productlist/index')
-        );
-
-        $subMenu->addItem(
-            new MenuItem('Klassiker', '/productlist/index')
-        );
-
-        $subMenu->addItem(
-            new MenuItem('Burger', '/productlist/index')
-        );
-
-        $this->mainMenu->addItem(
-            new MenuItem('Fingerfood', '/productlist/index',
-                $subMenu)
-        );
-//---------------------------------------------------------------------------------------------------------------------------
-        $subMenu = new MenuItemList();
-        $subMenu->addItem(
-            new MenuItem('Alle einsehnen', '/productlist/index')
-        );
-
-        $subMenu->addItem(
-            new MenuItem('Vegetarisch', '/productlist/index')
-        );
-
-        $subMenu->addItem(
-            new MenuItem('Klassiker', '/productlist/index')
-        );
-
-        $subMenu->addItem(
-            new MenuItem('Laktosefrei', '/productlist/index')
-        );
-
-        $subMenu->addItem(
-            new MenuItem('Warme', '/productlist/index')
-        );
-
-        $this->mainMenu->addItem(
-            new MenuItem('Salat', '/productlist/index',
-                $subMenu)
-        );
-//---------------------------------------------------------------------------------------------------------------------------
-        $subMenu = new MenuItemList();
-        $subMenu->addItem(
-            new MenuItem('Alle einsehnen', '/productlist/index')
-        );
-
-        $subMenu->addItem(
-            new MenuItem('Eis', '/productlist/index')
-        );
-
-        $subMenu->addItem(
-            new MenuItem('Klassiker', '/productlist/index')
-        );
-
-        $subMenu->addItem(
-            new MenuItem('Laktosefrei', '/productlist/index')
-        );
-
-        $subMenu->addItem(
-            new MenuItem('Warme', '/productlist/index')
-        );
-
-        $this->mainMenu->addItem(
-            new MenuItem('Desserts', '/productlist/index',
-                $subMenu)
-        );
-//---------------------------------------------------------------------------------------------------------------------------
-        $subMenu = new MenuItemList();
-        $subSubMenu = new MenuItemList();
-        $subMenu->addItem(
-            new MenuItem('Alle einsehnen', '/productlist/index')
-        );
-
-        $subMenu->addItem(
-            new MenuItem('Nicht Alkoholische', '/productlist/index')
-        );
-
-        $subSubMenu->addItem(
-            new MenuItem('Koffeinhaltig', '/productlist/index')
-        );
-
-        $subSubMenu->addItem(
-            new MenuItem('Koffeinfrei', '/productlist/index')
-        );
-
-        $subMenu->addItem(
-            new MenuItem('Alkoholische', '/productlist/index')
-        ); 
-
-        $this->mainMenu->addItem(
-            new MenuItem('Getränke', '/productlist/index',
-                $subMenu)
-        );
-    }
-    
-    public function getMainMenu()
-    {
-        return $this->mainMenu;
-    }
+			if ( empty( $subItems ) ) {                     // No subitems => no ItemList for this Entry
+				$entry = new MenuItem(
+					$elem->getName(),
+					$elem->getUrl()
+				);
+			}
+			else {                                       	// SubItems exists => save the ItemList
+				$entry = new MenuItem(
+					$elem->getName(),
+					$elem->getUrl(),
+					$subItems
+				);
+			}
+			$result->addItem( $entry );
+		}
+		return $result;
+	}
 }
+
+
+/*
+$this->mainMenu->addItem(
+    new MenuItem('Aktionen', '/campaigns/index')
+);
+
+$subMenu = new MenuItemList();
+$subMenu->addItem(
+    new MenuItem('Alle einsehnen', '/productlist/index')
+);
+
+$subMenu->addItem(
+    new MenuItem('Vegetarische Pizzas', '/productlist/index')
+);
+
+$subMenu->addItem(
+    new MenuItem('Klassiker', '/productlist/index')
+);
+
+$this->mainMenu->addItem(
+    new MenuItem('Pizza', '/productlist/index',
+        $subMenu)
+);
+//---------------------------------------------------------------------------------------------------------------------------
+$subMenu = new MenuItemList();
+$subMenu->addItem(
+    new MenuItem('Alle einsehnen', '/productlist/index')
+);
+
+$subMenu->addItem(
+    new MenuItem('Vegetarische', '/productlist/index')
+);
+
+$subMenu->addItem(
+    new MenuItem('Klassiker', '/productlist/index')
+);
+
+$subMenu->addItem(
+    new MenuItem('Überbackenes', '/productlist/index')
+);
+
+$subMenu->addItem(
+    new MenuItem('Saisonal', '/productlist/index')
+);
+
+$this->mainMenu->addItem(
+    new MenuItem('Nudeln', '/productlist/index',
+        $subMenu)
+);
+//---------------------------------------------------------------------------------------------------------------------------
+$subMenu = new MenuItemList();
+$subMenu->addItem(
+    new MenuItem('Alle einsehnen', '/productlist/index')
+);
+
+$subMenu->addItem(
+    new MenuItem('Anti Pasti', '/productlist/index')
+);
+
+$subMenu->addItem(
+    new MenuItem('Klassiker', '/productlist/index')
+);
+
+$subMenu->addItem(
+    new MenuItem('Burger', '/productlist/index')
+);
+
+$this->mainMenu->addItem(
+    new MenuItem('Fingerfood', '/productlist/index',
+        $subMenu)
+);
+//---------------------------------------------------------------------------------------------------------------------------
+$subMenu = new MenuItemList();
+$subMenu->addItem(
+    new MenuItem('Alle einsehnen', '/productlist/index')
+);
+
+$subMenu->addItem(
+    new MenuItem('Vegetarisch', '/productlist/index')
+);
+
+$subMenu->addItem(
+    new MenuItem('Klassiker', '/productlist/index')
+);
+
+$subMenu->addItem(
+    new MenuItem('Laktosefrei', '/productlist/index')
+);
+
+$subMenu->addItem(
+    new MenuItem('Warme', '/productlist/index')
+);
+
+$this->mainMenu->addItem(
+    new MenuItem('Salat', '/productlist/index',
+        $subMenu)
+);
+//---------------------------------------------------------------------------------------------------------------------------
+$subMenu = new MenuItemList();
+$subMenu->addItem(
+    new MenuItem('Alle einsehnen', '/productlist/index')
+);
+
+$subMenu->addItem(
+    new MenuItem('Eis', '/productlist/index')
+);
+
+$subMenu->addItem(
+    new MenuItem('Klassiker', '/productlist/index')
+);
+
+$subMenu->addItem(
+    new MenuItem('Laktosefrei', '/productlist/index')
+);
+
+$subMenu->addItem(
+    new MenuItem('Warme', '/productlist/index')
+);
+
+$this->mainMenu->addItem(
+    new MenuItem('Desserts', '/productlist/index',
+        $subMenu)
+);
+//---------------------------------------------------------------------------------------------------------------------------
+$subMenu = new MenuItemList();
+$subSubMenu = new MenuItemList();
+$subMenu->addItem(
+    new MenuItem('Alle einsehnen', '/productlist/index')
+);
+
+$subMenu->addItem(
+    new MenuItem('Nicht Alkoholische', '/productlist/index')
+);
+
+$subSubMenu->addItem(
+    new MenuItem('Koffeinhaltig', '/productlist/index')
+);
+
+$subSubMenu->addItem(
+    new MenuItem('Koffeinfrei', '/productlist/index')
+);
+
+$subMenu->addItem(
+    new MenuItem('Alkoholische', '/productlist/index')
+);
+
+$this->mainMenu->addItem(
+    new MenuItem('Getränke', '/productlist/index',
+        $subMenu)
+);
+
+*/
