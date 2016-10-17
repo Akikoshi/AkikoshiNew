e06f7c4957cb37987ef4ed0316f28ddac676b9d3
vieteo <oliver.viete@web4train.de>
2016-10-17 13:46:11 +0200

    change ProductListRepository

--- src/Services/ProductListService/Library/ProductListRepository.php
+++ src/Services/ProductListService/Library/ProductListRepository.php
@@ -13,27 +13,43 @@ use Class152\PizzaMamamia\Interfaces\ProductListRepositroryInterface;
 
 class ProductListRepository implements ProductListRepositroryInterface
 {
+    /** @var  array */
+    private $queryArray;
+    
+    /** @var  int */
     private $productId;
     
+    /** @var  string */
     private $productName;
     
+    /** @var  string */
     private $shortDescription;
     
+    /** @var  int */
     private $mediaFileId;
     
+    /** @var  string */
     private $typeOfProduct;
     
+    /** @var  int */
     private $productGroupId;
     
+    /** @var  float */
     private $grossPrice;
     
+    /** @var  int */
     private $vat;
     
+    /** @var  string */
     private $detailsUrl;
+    
+    //------------------------------------------------------------------------------------------------------------------
     
     public function __construct()
     {
         $this->databaseQuery();
+        $this->databaseQuery();
+        $this->setDetailsUrl();
     }
 
     private function databaseQuery()
@@ -43,52 +59,82 @@ class ProductListRepository implements ProductListRepositroryInterface
         where pd.`type` like "%ontain%" || pd.`type` like "%ingl%"
         order by pd.type;";
     }
-    
+
+    /**
+     * creates the Link to Details Site, is a hard Link can be refactored
+     */
     private function setDetailsUrl()
     {
         $this->detailsUrl = "/productdetails/index/".$this->productId;
     }
-    
+
+    /**
+     * @return int
+     */
     public function getProductId() :int
     {
         return $this->productId;
     }
 
+    /**
+     * @return string
+     */
     public function getProductName() :string
     {
         return $this->productName;
     }
 
+    /**
+     * @return string
+     */
     public function getShortDescription() :string
     {
         return $this->shortDescription;
     }
 
+    /**
+     * @return int
+     */
     public function getMediaFileId() :int
     {
         return $this->mediaFileId;
     }
 
+    /**
+     * @return string
+     */
     public function getTypeOfProduct() :string
     {
         return $this->typeOfProduct;
     }
 
+    /**
+     * @return int
+     */
     public function getProductGroupId() :int
     {
         return $this->productGroupId;
     }
 
+    /**
+     * @return float
+     */
     public function getGrossPrice() :float
     {
         return $this->grossPrice;
     }
 
+    /**
+     * @return int
+     */
     public function getVat() :int
     {
         return $this->vat;
     }
 
+    /**
+     * @return string
+     */
     public function getDetailUrl() :string
     {
         return $this->detailsUrl;
