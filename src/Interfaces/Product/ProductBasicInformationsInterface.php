d04b8454e09cf8fc1e4d03a6e6fe65e8633ab55b
trentschc <ranger1985@live.de>
2016-10-25 09:18:18 +0200

    add a getDefaultVariant method to the ProductBasicInformationsInterface

--- src/Interfaces/Product/ProductBasicInformationsInterface.php
+++ src/Interfaces/Product/ProductBasicInformationsInterface.php
@@ -1,63 +1,65 @@
 <?php
-	/**
-	 * Created by PhpStorm.
-	 * User: cbiedermann
-	 * Date: 21.10.2016
-	 * Time: 08:35
-	 */
+/**
+ * Created by PhpStorm.
+ * User: cbiedermann
+ * Date: 21.10.2016
+ * Time: 08:35
+ */
 
-	namespace Class152\PizzaMamamia\Interfaces\Product;
+namespace Class152\PizzaMamamia\Interfaces\Product;
 
-	use Class152\PizzaMamamia\Interfaces\MediaFileInterface;
+use Class152\PizzaMamamia\Interfaces\MediaFileInterface;
 
-	/**
-	 * Interface ProductBasicInformationsInterface
-	 * for a essential View of any products
-	 *
-	 * @package Class152\PizzaMamamia\Interfaces\Product
-	 */
-	interface ProductBasicInformationsInterface
-	{
-		/**
-		 * @return string
-		 */
-		public function getId();
+/**
+ * Interface ProductBasicInformationsInterface
+ * for a essential View of any products
+ *
+ * @package Class152\PizzaMamamia\Interfaces\Product
+ */
+interface ProductBasicInformationsInterface
+{
+    /**
+     * @return string
+     */
+    public function getId() : string;
 
-		/**
-		 * @return string
-		 */
-		public function getName();
+    /**
+     * @return string
+     */
+    public function getName() : string;
 
-		/**
-		 * @return MediaFileInterface
-		 */
-		public function getThumb();
+    /**
+     * @return MediaFileInterface
+     */
+    public function getThumb() : MediaFileInterface;
 
-		/**
-		 * text type depends of current scope
-		 *
-		 * @return string
-		 */
-		public function getDescription();
+    /**
+     * text type depends of current scope
+     *
+     * @return string
+     */
+    public function getDescription() : string;
 
-		/**
-		 * @return bool
-		 */
-		public function hasDescription();
+    /**
+     * @return bool
+     */
+    public function hasDescription() : bool;
 
-		/**
-		 * @return bool
-		 */
-		public function isSingle();
+    /**
+     * @return bool
+     */
+    public function isSingle() : bool;
 
-		/**
-		 * @return bool
-		 */
-		public function hasVariants();
+    /**
+     * @return bool
+     */
+    public function hasVariants() : bool;
 
-		/**
-		 * @return ProductVariantsIteratorInterface
-		 */
-		public function getVariants();
+    /**
+     * @return ProductVariantsIteratorInterface
+     */
+    public function getVariants() : ProductVariantsIteratorInterface;
 
-	}
+    public function getDefaultVariant() : ProductVariantInterface;
+
+}
