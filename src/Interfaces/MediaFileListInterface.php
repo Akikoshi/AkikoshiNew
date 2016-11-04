c639ca02ed93a6ccc50a2ee05f5bbf2742144ce0
frankenfeldtp <peter.frankenfeldt@web4train.de>
2016-11-04 12:04:16 +0100

    UPDATE: MediaFile - Link (LinkInterface)
    UPDATE: MediaFileList
    - ADD: default MediaFile for 'no image found'
    - ADD: hasItems
    - ADD: getDefault
    
    ADD: MediaFileInterface
    ADD: MediaFileListInterface
    ADD: Exception

---
+++ src/Interfaces/MediaFileListInterface.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: frankenfeldtp
+ * Date: 04.11.2016
+ * Time: 10:46
+ */
+namespace Class152\PizzaMamamia\Interfaces;
+
+interface MediaFileListInterface extends AbstractIteratorInterface
+{
+    /**
+     * overloads the current method for restricted type of return value
+     *
+     * @return MediaFileInterface
+     */
+    public function current() : MediaFileInterface;
+
+    /**
+     * Returns FALSE if list empty.
+     * Returns TRUE if list not empty.
+     *
+     * @return bool
+     */
+    public function hasItems() : bool;
+
+    /**
+     * Return the default MediaFile.
+     * The default thumb is the thumb of the default MediaFile.
+     *
+     * @return MediaFileInterface
+     */
+    public function getDefault(): MediaFileInterface;
+}
---
+++ src/Library/MediaFile.php
@@ -0,0 +1,242 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: johannesj
+ * Date: 17.10.2016
+ * Time: 13:12
+ */
+
+namespace Class152\PizzaMamamia\Library;
+
+use Class152\PizzaMamamia\Interfaces\LinkInterface;
+use Class152\PizzaMamamia\Interfaces\MediaFileInterface;
+
+class MediaFile implements MediaFileInterface
+{
+    /**
+     * Hold the Id of the MediaFile
+     *
+     * @var int
+     */
+    private $id;
+
+    /**
+     * Hold the mime-type of the images.
+     * All images must have the same mime-type.
+     *
+     * @var string
+     */
+    private $mime;
+
+    /**
+     * Hold the height in pixel of the standard image version.
+     *
+     * @var int
+     */
+    private $height;
+
+    /**
+     * Hold the width in pixel of the standard image version.
+     *
+     * @var int
+     */
+    private $width;
+
+    /**
+     * Hold the height in pixel of the thumb image version.
+     *
+     * @var int
+     */
+    private $thumbHeight;
+
+    /**
+     * Hold the width in pixel of the thumb image version.
+     *
+     * @var int
+     */
+    private $thumbWidth;
+
+    /**
+     * Hold the height in pixel of the large image version.
+     *
+     * @var int
+     */
+    private $bigHeight;
+
+    /**
+     * Hold the width in pixel of the large image version.
+     *
+     * @var int
+     */
+    private $bigWidth;
+
+    /**
+     * Hold url of the normal image version.
+     *
+     * @var LinkInterface
+     */
+    private $url;
+
+    /**
+     * Hold url of the thumb image version.
+     *
+     * @var LinkInterface
+     */
+    private $thumbUrl;
+
+    /**
+     * Hold url of the large image version.
+     *
+     * @var LinkInterface
+     */
+    private $bigUrl;
+
+    /**
+     * Hold the title-text for this MediaFile.
+     *
+     * @var string
+     */
+    private $titleTag;
+
+    /**
+     * Hold the alt-text for this MediaFile.
+     *
+     * @var string
+     */
+    private $altTag;
+
+
+    public function __construct(
+        $id,
+        $mime,
+        $height,
+        $width,
+        $thumbHeight,
+        $thumbWidth,
+        $bigHeight,
+        $bigWidth,
+        $url,
+        $thumbUrl,
+        $bigUrl,
+        $titleTag,
+        $altTag)
+    {
+        $this->id = $id;
+        $this->mime = $mime;
+        $this->height = $height;
+        $this->width = $width;
+        $this->thumbHeight = $thumbHeight;
+        $this->thumbWidth = $thumbWidth;
+        $this->bigHeight = $bigHeight;
+        $this->thumbUrl = $bigWidth;
+        $this->bigWidth = $url;
+        $this->url = $thumbUrl;
+        $this->bigUrl = $bigUrl;
+        $this->titleTag = $titleTag;
+        $this->altTag = $altTag;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId() : int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMime() : string
+    {
+        return $this->mime;
+    }
+
+    /**
+     * @return int
+     */
+    public function getHeight() : int
+    {
+        return $this->height;
+    }
+
+    /**
+     * @return int
+     */
+    public function getWidth() : int
+    {
+        return $this->width;
+    }
+
+    /**
+     * @return int
+     */
+    public function getThumbHeight() : int
+    {
+        return $this->thumbHeight;
+    }
+
+    /**
+     * @return int
+     */
+    public function getThumbWidth() : int
+    {
+        return $this->thumbWidth;
+    }
+
+    /**
+     * @return int
+     */
+    public function getBigHeight() : int
+    {
+        return $this->bigHeight;
+    }
+
+    /**
+     * @return int
+     */
+    public function getBigWidth() : int
+    {
+        return $this->bigWidth;
+    }
+
+    /**
+     * @return LinkInterface
+     */
+    public function getUrl() : LinkInterface
+    {
+        return $this->url;
+    }
+
+    /**
+     * @return LinkInterface
+     */
+    public function getThumbUrl() : LinkInterface
+    {
+        return $this->thumbUrl;
+    }
+
+    /**
+     * @return LinkInterface
+     */
+    public function getBigUrl() : LinkInterface
+    {
+        return $this->bigUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitleTag() : string
+    {
+        return $this->titleTag;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAltTag() : string
+    {
+        return $this->altTag;
+    }
+}
