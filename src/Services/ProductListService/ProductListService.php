0eaf64bc9bd7fc1f908d50717358b1d0ca7f9969
vieteo <oliver.viete@web4train.de>
2016-11-07 09:15:40 +0100

    ProductList comes to Front

--- src/Services/ProductListService/ProductListService.php
+++ src/Services/ProductListService/ProductListService.php
@@ -10,29 +10,17 @@ namespace Class152\PizzaMamamia\Services\ProductListService;
 
 
 use Class152\PizzaMamamia\Services\ProductListService\Filter\ProductListFilter;
+use Class152\PizzaMamamia\Services\ProductListService\Iterators\ProductList;
 use Class152\PizzaMamamia\Services\ProductListService\Library\FilterListFactory;
 use Class152\PizzaMamamia\Services\ProductListService\Factories\ProductListFactory;
 use Class152\PizzaMamamia\Services\ProductListService\Library\SortListFactory;
 
 class ProductListService
 {
-    public function getProductList()
+    public function getProductList() :ProductList
     {
         $productListFilter = new ProductListFilter(null, false, false);
         $productFactory = new ProductListFactory($productListFilter);
         return $productFactory->getInstance();
     }
-
-    //Todo: check if obsolete
-//    public function getFilterList()
-//    {
-//        $filterListFactory = new FilterListFactory();
-//        return $filterListFactory->getFilterList();
-//    }
-//
-//    public function getSortList()
-//    {
-//        $sortListFactory = new SortListFactory();
-//        return $sortListFactory->getSortList();
-//    }
 }
